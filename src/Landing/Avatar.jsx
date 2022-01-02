5b36c4cddfc4766663137933a1878b2f1e5e7760
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2022-01-03 01:04:01 +0700

    update alt img

--- src/Landing/Avatar.jsx
+++ src/Landing/Avatar.jsx
@@ -4,7 +4,7 @@ export default class Avatar extends React.Component {
 
     render() {
         return (
-            <img src={me} className="border-4 border-white object-cover w-[180px] h-[180px] rounded-full mb-5" />
+            <img src={me} className="border-4 border-white object-cover w-[180px] h-[180px] rounded-full mb-5" alt="Hilmi Mubarok" />
         )
     }
     
