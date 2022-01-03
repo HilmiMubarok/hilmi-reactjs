3f4a34d8b3fd07cb9fe91699deb4785801bc720b
HilmiMubarok <--get>
2022-01-03 15:15:54 +0700

    task basic component done

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -4,17 +4,30 @@ import Avatar from "./Avatar";
 import Card from "./Card";
 
 class Home extends React.Component {
+    
+    state = {
+        value : 0
+    }
 
+    backgrounds = [
+        "bg-white", "bg-red-600", "bg-emerald-600", "bg-orange-600",
+        "bg-amber-600", "bg-yellow-600", "bg-lime-600", "bg-green-600",
+    ]
+    randomize = () => {
+        this.setState({
+            value : this.backgrounds[this.random()]
+        })
+        // return this.state.value
+    }
 
-    randomBg() {
-        const bg = [
-            "bg-white", "bg-red-600", "bg-emerald-600", "bg-orange-600",
-            "bg-amber-600", "bg-yellow-600", "bg-lime-600", "bg-green-600",
-        ]
-
-        return bg[Math.floor(Math.random() * bg.length)]
+    random = () => {
+        return Math.floor(Math.random() * this.backgrounds.length)
     }
     render () {
+
+        // console.log(this.state.value[this.random()])
+        console.log(this.random())
+        // console.log(this.backgrounds.length)
 
 
         return (
@@ -27,23 +40,23 @@ class Home extends React.Component {
                 <div className="container mt-[100px]">
                     <div className="flex justify-between">
                         <div className="p-3 text-white text-xl">Materi</div>
-                        <button className="p-2 rounded bg-red-900 text-white ">
+                        <button className="p-2 rounded bg-red-900 text-white" onClick={this.randomize}>
                             Randomize bg card
                         </button>
                     </div>
                     <div className="grid grid-cols-4 gap-4 mt-5">
 
-                        <Card number="1" bg={this.randomBg()} title="Basic Components" />
+                        <Card number="1" bg={this.backgrounds[this.random()]} title="Basic Components" />
 
-                        <Card number="2" bg={this.randomBg()} title="Styling Components" />
+                        <Card number="2" bg={this.backgrounds[this.random()]} title="Styling Components" />
 
-                        <Card number="3" bg={this.randomBg()} title="Reusable Components" />
+                        <Card number="3" bg={this.backgrounds[this.random()]} title="Reusable Components" />
 
-                        <Card number="4" bg={this.randomBg()} title="Rendering Variable" />
+                        <Card number="4" bg={this.backgrounds[this.random()]} title="Rendering Variable" />
 
-                        <Card number="5" bg={this.randomBg()} title="Form and Validation" />
+                        <Card number="5" bg={this.backgrounds[this.random()]} title="Form and Validation" />
 
-                        <Card number="6" bg={this.randomBg()} title="Lifecycle Components" />
+                        <Card number="6" bg={this.backgrounds[this.random()]} title="Lifecycle Components" />
                     </div>
                 </div>
 
