77808dbbe274dad5053d42625ffaa4fe3cb83323
HilmiMubarok <--get>
2022-01-14 15:15:43 +0700

    delete home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Portfolio from './Portfolio';
-// import Home from './Home';
 
 function App() {
 return (
 	<div className="App">
-		{/* <Home/> */}
 		<Portfolio/>
 	</div>
 );
