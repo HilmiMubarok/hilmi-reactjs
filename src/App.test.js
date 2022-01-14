3404e9d91bdd84c33491b20c371154704e1c4365
HilmiMubarok <--get>
2022-01-14 18:37:14 +0700

    edit app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Articles from './Articles';
+import {
+  BrowserRouter, Routes,
+  Route
+} from "react-router-dom";
 
 test('renders learn react link', () => {
-  render(<App />);
+  // render(<App />);
+  render(
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="articles" element={<Articles />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
   screen.debug()
